6446b4b91e278340a4b1d639636ad5983cd123c1
JordynK91 <JordynK91@gmail.com>
2018-01-17 23:12:13 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+//his is the first exercise
+
+/*var button2 = document.getElementById('button2')
+var button1 = document.getElementById('button1')
+
+button1.addEventListener('click', pressButton1)
+button2.addEventListener('click', pressButton2)
+
+function pressButton1(){
+	var output = "I am right!"
+	result.innerHTML = output
+	result.style.fontSize="30px";
+	result.style.backgroundColor = "red";
+	}
+
+function pressButton2(){
+	var output = "No! I am right"
+	result.innerHTML = output
+	result.style.fontSize="30px";
+	result.style.backgroundColor="blue";
+}
+*/
+
+//this is the second exercise
+
+
+/* var box = document.getElementById('box')
+
+box.addEventListener('mouseover', hover)
+
+function hover(){
+	alert('I told you not to hover over me')
+}
+*/
+
+// this is the third exericse which is HARD and I have nothing
+
+
+//this is the fourth exercise which ALSO ISN'T WORKING
+
+/*
+
+var submit = document.getElementById('submit')
+
+submit.addEventListener('click', check)
+
+var x = document.getElementById("username").value
+var y = document.getElementById("password").value
+
+function check ()
+		{if ((x.length < 14) && (y === 12345678)) {
+			alert("correct username and password")
+		}
+		else {
+			alert("incorrect username and password")
+		}
+}	
